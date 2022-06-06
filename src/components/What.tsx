6eb18e6ac04f17f94918e99ee750585d1c5d11b9
prince-ao <princead@tutanota.com>
2022-06-06 12:59:55 -0400

    Feat: Done with what component

--- src/components/What.tsx
+++ src/components/What.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const What = () => {
   return (
-    <div>
+    <div className="what">
       <h1>What's Hack-A-Project?</h1>
-      <p>A CUNY club that organizes various collaborative, project based events for diverse communities.</p>
+      <p>A CUNY based club that organizes various collaborative, project and technology based events for a diverse community.</p>
     </div>
   )
 }
