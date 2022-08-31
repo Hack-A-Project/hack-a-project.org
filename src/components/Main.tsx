a1c7183136cb02f40d7dda74d2217bb3e75644fa
prince-ao <prince.addo@cix.csi.cuny.edu>
2022-08-31 11:05:27 -0400

    added discord to website

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import logo from "../assets/hack_white.png"
 import linkedin from '../assets/linkedin.png'
+import discord from '../assets/discord_logo.png'
 
 const Main = () => {
   return (
     <div id="main" className="main">
       <img src={logo} alt="" />
       <div className="socials">
-        <img src={linkedin} onClick={() => window.open('https://www.linkedin.com/in/hack-a-project/')} alt="linkedin logo" />
+        <img className="linkedin_logo" src={linkedin} onClick={() => window.open('https://www.linkedin.com/in/hack-a-project/')} alt="linkedin logo" />
+        <img className="discord_logo" src={discord} onClick={() => window.open('https://discord.gg/WAWdWchbHz')} alt="discord logo" />
       </div>
       <button onClick={() => window.open("https://csi.campuslabs.com/engage/organization/hack_a_project")}>Join Club</button>
     </div>
